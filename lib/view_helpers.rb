dd5170a609e8f891f1c7beeb7386c8a6ae2a2fb0
Andri Möll <andri@dot.ee>
2014-11-20 12:35:05 +0200

    Add boilerplate for guidelines.

--- lib/view_helpers.rb
+++ lib/view_helpers.rb
@@ -4,3 +4,7 @@ def stylesheet_tag(path)
   path = "/assets/stylesheets/#{path}.css" if path.is_a? Symbol
   %(<link rel="stylesheet" type="text/css" href="#{path}" media="screen">)
 end
+
+def items_by_kind(kind)
+  items.select {|item| item[:kind] == kind }
+end
