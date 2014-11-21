aee47ee6a241010ab1488b4c78152bbd5957c450
Andri Möll <andri@dot.ee>
2014-11-21 15:23:00 +0200

    Add print stylesheet.

--- lib/view_helpers.rb
+++ lib/view_helpers.rb
@@ -2,7 +2,7 @@ include Nanoc::Helpers::Rendering
 
 def stylesheet_tag(path)
   path = "/assets/stylesheets/#{path}.css" if path.is_a? Symbol
-  %(<link rel="stylesheet" type="text/css" href="#{path}" media="screen">)
+  %(<link rel="stylesheet" type="text/css" href="#{path}">)
 end
 
 def items_by_kind(kind)
